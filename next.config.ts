a130ee78f3f7e7cadeba81475bebfb27a5a77a97
Temp445 <acetemp345@gmail.com>
2025-07-24 12:50:41 +0530

    testing

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   basePath: '/ace-project',
-  assetPrefix: '/aceproject',
+  assetPrefix: '/ace-project',
   trailingSlash: true,
     images: {
         remotePatterns: [
