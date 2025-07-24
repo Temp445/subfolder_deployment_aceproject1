870c64d63852c615e26126dbbbc86f2cd6ed987b
Temp445 <acetemp345@gmail.com>
2025-07-24 15:32:36 +0530

    testing

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -3,39 +3,39 @@
 import Marquee from "react-fast-marquee";
 import Image, { StaticImageData } from "next/image";
 
-import img0 from "@/assets/image-0.png";
-import img1 from "@/assets/image-1.png";
-import img2 from "@/assets/image-2.png";
-import img3 from "@/assets/image-3.png";
-import img4 from "@/assets/image-4.png";
-import img5 from "@/assets/image-5.png";
-import img6 from "@/assets/image-6.png";
-import img7 from "@/assets/image-7.png";
-import img8 from "@/assets/image-8.png";
-import img9 from "@/assets/image-9.png";
-import img10 from "@/assets/image-10.png";
-import img11 from "@/assets/image-11.png";
-import img12 from "@/assets/image-12.png";
-import img13 from "@/assets/image-13.png";
-import img14 from "@/assets/image-14.png";
-import img15 from "@/assets/image-15.png";
-import img16 from "@/assets/image-16.png";
-import img17 from "@/assets/image-17.png";
-import img18 from "@/assets/image-18.png";
-import img19 from "@/assets/image-19.png";
-import img20 from "@/assets/image-20.png";
-import img21 from "@/assets/image-21.png";
-import img22 from "@/assets/image-22.png";
-import img23 from "@/assets/image-23.png";
-import img24 from "@/assets/image-24.png";
-import img25 from "@/assets/image-25.png";
-import img26 from "@/assets/image-26.png";
-import img27 from "@/assets/image-27.png";
-import img28 from "@/assets/image-28.png";
-import img29 from "@/assets/image-29.png";
+import img0 from "@/assets/ClientImages/image-0.png";
+import img1 from "@/assets/ClientImages/image-1.png";
+import img2 from "@/assets/ClientImages/image-2.png";
+import img3 from "@/assets/ClientImages/image-3.png";
+import img4 from "@/assets/ClientImages/image-4.png";
+import img5 from "@/assets/ClientImages/image-5.png";
+import img6 from "@/assets/ClientImages/image-6.png";
+import img7 from "@/assets/ClientImages/image-7.png";
+import img8 from "@/assets/ClientImages/image-8.png";
+import img9 from "@/assets/ClientImages/image-9.png";
+import img10 from "@/assets/ClientImages/image-10.png";
+import img11 from "@/assets/ClientImages/image-11.png";
+import img12 from "@/assets/ClientImages/image-12.png";
+import img13 from "@/assets/ClientImages/image-13.png";
+import img14 from "@/assets/ClientImages/image-14.png";
+import img15 from "@/assets/ClientImages/image-15.png";
+import img16 from "@/assets/ClientImages/image-16.png";
+import img17 from "@/assets/ClientImages/image-17.png";
+import img18 from "@/assets/ClientImages/image-18.png";
+import img19 from "@/assets/ClientImages/image-19.png";
+import img20 from "@/assets/ClientImages/image-20.png";
+import img21 from "@/assets/ClientImages/image-21.png";
+import img22 from "@/assets/ClientImages/image-22.png";
+import img23 from "@/assets/ClientImages/image-23.png";
+import img24 from "@/assets/ClientImages/image-24.png";
+import img25 from "@/assets/ClientImages/image-25.png";
+import img26 from "@/assets/ClientImages/image-26.png";
+import img27 from "@/assets/ClientImages/image-27.png";
+import img28 from "@/assets/ClientImages/image-28.png";
+import img29 from "@/assets/ClientImages/image-29.png";
 
 const Clients: React.FC = () => {
-  const logos: StaticImageData[] = [
+  const logos = [
     img0, img1, img2, img3, img4, img5, img6, img7, img8, img9,
     img10, img11, img12, img13, img14, img15, img16, img17, img18,
     img19, img20, img21, img22, img23, img24, img25, img26, img27,
